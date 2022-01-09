edc3cf4073dc5f9f13aeddc2e36b58c21583667a
garrettdurbin <garrettdurbin@gmail.com>
2022-01-09 13:22:09 -0600

    Added useStates users and loading

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,19 +1,41 @@
-import {useEffect} from 'react'
+import { useEffect, useState } from 'react'
 
 function UserResults() {
-  useEffect(() => {
+  // The "name" of the useStates is how you access them in the return part of the component.
+  const [users, setUsers] = useState([])
+  const [loading, setLoading] = useState(true)
 
+  useEffect(() => {
+    fetchUsers()
+    console.log('fetchUsers Fired from useEffect')
   }, [])
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`)
+    console.log('fetchUsers fired from fetchUsers')
+    const response = await fetch('https://api.github.com/users', {
+      headers: {
+        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    })
+
+    const data = await response.json()
+
+    setUsers(data)
+    setLoading(false)
   }
 
-  return (
-    <div>
-      user results
-    </div>
-  )
+  if(!loading) {
+    return (
+      <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    )
+  } else {
+    return <h3>Loading...</h3>
+  }
+  
 }
 
 export default UserResults
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import UserResults from "../components/users/UserResults"
 function Home() {
   return (
     <div>
+      
       {/* Search Component will go here */}
       <UserResults />
     </div>
