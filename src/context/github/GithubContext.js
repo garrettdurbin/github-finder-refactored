73239993bc77e99be6b2dd7e897cf1e137017d14
garrettdurbin <garrettdurbin@gmail.com>
2022-01-13 14:55:37 -0600

    Switched form useState hook to just reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,17 +1,26 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer } from 'react'
+import githubReducer from './GithubReducer'
 
 const GithubContext = createContext()
 
 // I don't think I've ever had these variables working. So I'll have to use the actual github url and access token below.
-const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
+// const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
 // Traversy said we'll have to export a provider. I think this is just a standard thing you have to do when using Context in React.
 // GithubProvider is going to take in props, and we want the children from that...he then said, that's basically what we surround with the provider...but that last part doesn't make sense yet.
 export const GithubProvider = ({ children }) => {
   // The "name" of the useStates is how you access them in the return part of the component.
-  const [users, setUsers] = useState([])
-  const [loading, setLoading] = useState(true)
+  // const [users, setUsers] = useState([])
+  // const [loading, setLoading] = useState(true)
+
+  // useReducer version of state
+  const initialState = {
+    users: [],
+    loading: true
+  }
+
+  const [state, dispatch] = useReducer(githubReducer, initialState)
 
   const fetchUsers = async () => {
     console.log('fetchUsers fired from fetchUsers')
@@ -23,14 +32,18 @@ export const GithubProvider = ({ children }) => {
 
     const data = await response.json()
 
-    setUsers(data)
-    setLoading(false)
+    dispatch({
+      type: 'GET_USERS',
+      payload: data,
+    })
   }
 
   return (
+    // Anything I want to get out somewhere else using context I have to include here within the "Provider"
     <GithubContext.Provider value={{
-        users,
-        loading,
+        // not sure why this has to be "state.users" rather than "users"...Answer: I used to be defining state line this with useState: const [users, setUsers] = useState([]). HOWEVER, now I'm getting the values from "const [state, dispatch] = useReducer(githubReducer, initialState)"
+        users: state.users,
+        loading: state.loading,
         fetchUsers,
       }}>
       {children}
